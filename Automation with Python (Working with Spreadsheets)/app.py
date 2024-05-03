2fdbcb61cc4b38f4110da0bedfa6db052da65e1a
Fojan Babaali <Fojan.Babaali@ad.carmanah.com>
2024-05-03 16:23:22 -0700

    Done

--- Automation with Python (Working with Spreadsheets)/app.py
+++ Automation with Python (Working with Spreadsheets)/app.py
@@ -1,5 +1,5 @@
 import openpyxl
-from colors import CYAN,MAGENTA,BRIGHT_YELLOW,RED,RESET
+from colors import CYAN,MAGENTA,BRIGHT_YELLOW,RED,BRIGHT_RED,RESET
 # Read an excel file from the current directory on your local machine.
 # sample_data_all = SampleData.xlsx with all of the sheets inside of it.
 sample_data_all = openpyxl.load_workbook("SampleData.xlsx")
@@ -18,18 +18,31 @@ def generate_number_of_purchases_per_buyer():
             # Adding new buyer to number_of_purchases_per_buyer
             number_of_purchases_per_buyer[buyer] = 1
 generate_number_of_purchases_per_buyer()
-print("-------------- ✿ --------------")
 print(MAGENTA + "1- Total number of purchases per buyer." 
-      + BRIGHT_YELLOW + "\nAns: "+ CYAN + f"{number_of_purchases_per_buyer}" + RESET)
+      + BRIGHT_YELLOW + "\n✿ Answer: "+ CYAN + f"{number_of_purchases_per_buyer}" + RESET)
 print(MAGENTA + "\n2- Total number of purchases per buyer sorted alphabetically." 
-      + BRIGHT_YELLOW + "\nAns: "+ CYAN + f"{dict(sorted(number_of_purchases_per_buyer.items()))}" + RESET)
+      + BRIGHT_YELLOW + "\n✿ Answer: "+ CYAN + f"{dict(sorted(number_of_purchases_per_buyer.items()))}" + RESET)
 print(MAGENTA + "\n3- How many buyers in the excel sheet?" 
-      + BRIGHT_YELLOW + "\nAns: " + CYAN + f"{len(number_of_purchases_per_buyer)}" + RESET)
+      + BRIGHT_YELLOW + "\n✿ Answer: " + CYAN + f"{len(number_of_purchases_per_buyer)}" + RESET)
 print(MAGENTA + "\n4- Sort the buyers name alphabetically." 
-      + BRIGHT_YELLOW + "\nAns: "+ CYAN + f"{sorted(list(set(buyers)))}" + RESET)
-print("-------------- ✿ --------------\n")
+      + BRIGHT_YELLOW + "\n✿ Answer: "+ CYAN + f"{sorted(list(set(buyers)))}" + RESET)
 
-# Find a buyer's name and return the number of total purchases that he/she made :
+
+# ------ ✿ Functions ✿ -------
+
+# Find the minimum number of total purchases and return bueyrs name who made this minimum amount of purchases :
+def find_minimum_purchase():
+    minimum_purchase = min(number_of_purchases_per_buyer.values())
+    buyers_with_minimum_purchases = {}
+    for key in number_of_purchases_per_buyer:
+        if number_of_purchases_per_buyer[key] == minimum_purchase:
+            buyers_with_minimum_purchases[key] = minimum_purchase
+    print(BRIGHT_RED +"Minimum paurchases is "+ f"{minimum_purchase} 👉 " 
+          + f"{buyers_with_minimum_purchases}" + RESET)
+    
+# -----------------------------
+    
+# Get a buyer's name from user and find/return the number of total purchases that he/she made :
 def find_number_of_purchases_by_buyer_name():
     a_buyer_name = input("Please enter the buyer's name to see how many purchases he/she made so far in total:")
     if a_buyer_name in number_of_purchases_per_buyer:
@@ -37,4 +50,5 @@ def find_number_of_purchases_by_buyer_name():
         print(BRIGHT_YELLOW + f"{a_buyer_name} has already made {total_purchases} purchases in total." + RESET)
     else:
         print(RED + "Not existed buyer." + RESET)
+
 
